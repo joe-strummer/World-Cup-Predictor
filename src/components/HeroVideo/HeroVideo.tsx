0e6109456f198a8b74b82eee998aeb2eb64d375b
Peji <peji.faghihi@news.co.uk>
2026-05-17 18:28:37 +0100

    CSS tinkering, and performance improvements based on lighthouse score

--- src/components/HeroVideo/HeroVideo.tsx
+++ src/components/HeroVideo/HeroVideo.tsx
@@ -3,7 +3,7 @@ import './HeroVideo.css'
 export function HeroVideo() {
   return (
     <section className="hero-video">
-      <video src="/World_Cup_Players_To_Watch.mp4" autoPlay loop muted playsInline className="hero-video__video" />
+      <video src="/World_Cup_Players_To_Watch.mp4" poster="/videoframe.webp" autoPlay loop muted playsInline className="hero-video__video" />
       <div className="hero-video__overlay" />
       <div className="hero-video__content">
         <h1 className="hero-video__heading">Path to Glory</h1>
