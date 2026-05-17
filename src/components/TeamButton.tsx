261f6fe973e32ef549549803aca1bbdf3d81edfa
Peji <peji.faghihi@news.co.uk>
2026-05-17 10:53:29 +0100

    Add flag to team button, add aria label of the country name

--- src/components/TeamButton.tsx
+++ src/components/TeamButton.tsx
@@ -1,4 +1,5 @@
 import './TeamButton.css'
+import { COUNTRY_NAMES } from '../data/countryNames'
 
 export type TeamButtonState = 'unselected' | 'selected' | 'greyed'
 
@@ -13,9 +14,20 @@ export function TeamButton({
   state = 'unselected',
   onClick,
 }: TeamButtonProps) {
+  const countryName = COUNTRY_NAMES[label] || label
+  const flagSrc = `/flags/${label}.svg`
+
   return (
-    <button type="button" className={`team-button team-button--${state}`} onClick={onClick}>
-      {label}
+    <button
+      type="button"
+      className={`team-button team-button--${state}`}
+      onClick={onClick}
+      aria-label={countryName}
+    >
+      {label !== 'Draw' && (
+        <img src={flagSrc} alt="" className="team-button__flag" />
+      )}
+      <span className="team-button__label">{label}</span>
     </button>
   )
 }
